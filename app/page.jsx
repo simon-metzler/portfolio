023bb69dfb398cee78f5684584489d21d8d39842
simon-metzler <simon.metzler@student.htldornbirn.at>
2024-07-14 20:19:57 +0200

    basic changes to main page

--- app/page.jsx
+++ app/page.jsx
@@ -3,24 +3,28 @@ import Timeline from "./components/Timeline";
 
 export default function Home() {
   return (
-    <main className="flex flex-col justify-center">
-      <h1>Heading</h1>
-      <div className="flex flex-row items-center p-24 gap-10">
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit
-          voluptatibus debitis labore explicabo odit hic necessitatibus,
-          possimus maiores vero molestias in voluptate unde. Consequatur hic
-          earum inventore perferendis labore modi!
-        </p>
-        <Image
-          src="/weasel.png"
-          alt="Weasel"
-          width={200}
-          height={200}
-          className="rounded"
-        />
+    <main className="flex justify-center">
+      <div className="card bg-base-100 w-2/3 shadow-xl">
+        <div className="card-body">
+          <h1 className="text-5xl">About me</h1>
+          <div className="flex flex-row items-center gap-10">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit
+              voluptatibus debitis labore explicabo odit hic necessitatibus,
+              possimus maiores vero molestias in voluptate unde. Consequatur hic
+              earum inventore perferendis labore modi!
+            </p>
+            <Image
+              src="/weasel.png"
+              alt="Weasel"
+              width={200}
+              height={200}
+              className="rounded"
+            />
+          </div>
+          <Timeline />
+        </div>
       </div>
-      <Timeline />
     </main>
   );
 }
